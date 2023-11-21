ec9355b7e46733605280129ef737123a554fbe5b
john-apton <john@aptonworks.com>
2023-11-21 22:33:17 +0530

    React native initial - stock code removed

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView style={{}}>
+      <ScrollView>
+        <View>
+          <Text>JOhn</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
+
+export const style = StyleSheet.create({});
